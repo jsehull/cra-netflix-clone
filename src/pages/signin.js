16566ad78bd27fc405139fc1d0be25226d924429
Jesse Hull <jse.hull@gmail.com>
2020-11-09 13:43:54 -0700

    feat: add signup section

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,16 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import { FirebaseContext } from '../context/firebase'
 import { HeaderContainer } from '../containers/header'
 import { FooterContainer } from '../containers/footer'
 import { Form } from '../components'
+import * as ROUTES from '../constants/routes'
 
 export default function SignIn() {
+  const { history } = useHistory()
+  const { firebase } = useContext(FirebaseContext)
   const [emailAddress, setEmailAddress] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
 
   const isInvalid = password === '' || emailAddress === ''
 
-  function handleSignIn() {}
+  function handleSignIn(e) {
+    e.preventDefault()
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE)
+      })
+      .catch(error => {
+        setEmailAddress('')
+        setPassword('')
+        setError(error.message)
+      })
+  }
 
   return (
     <>
