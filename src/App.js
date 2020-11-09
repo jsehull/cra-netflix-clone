25c2e75c6416797a04bca1e0ad615685a6a9f3af
Jesse Hull <jse.hull@gmail.com>
2020-11-09 14:15:59 -0700

    feat: add routes based on login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Home, Browse, SignUp, SignIn } from './pages'
 import * as ROUTES from './constants/routes'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 
 export default function App() {
+  const user = null
+
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <SignUp />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <SignIn />
-      </Route>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+          exact
+        >
+          <Home />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+          exact
+        >
+          <SignUp />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+          exact
+        >
+          <SignIn />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   )
 }
